24094b9d28c3618bc08e5d76025cae3b8819b2e3
sheltonma <shelton.xiaoteng.ma@gmail.com>
2025-04-14 14:09:14 +0800

    init next-app and fastify-api

---
+++ README.md
@@ -0,0 +1,10 @@
+# Shelton Scaffold
+
+## 项目包含
+
+- pnpm workspace 多包管理
+- packages/trpc-types：共享 zod schema 和类型定义
+- apps/web：Next.js 15 前端，tRPC 客户端集成
+- apps/api：Fastify 后端服务，tRPC 服务端实现
+- prisma：PostgreSQL schema 初始化
+- Dockerfile 和 docker-compose.yml：用于部署后端 + 数据库
---
+++ apps/fastify-api/src/index.ts
@@ -0,0 +1,32 @@
+import {
+  fastifyTRPCPlugin,
+  FastifyTRPCPluginOptions,
+} from "@trpc/server/adapters/fastify";
+import fastify from "fastify";
+import { createContext } from "./context";
+import { appRouter, AppRouter } from "./router";
+
+const server = fastify({
+  maxParamLength: 5000,
+});
+
+server.register(fastifyTRPCPlugin, {
+  prefix: "/trpc",
+  trpcOptions: {
+    router: appRouter,
+    createContext,
+    onError({ path, error }) {
+      // report to error monitoring
+      console.error(`Error in tRPC handler on path '${path}':`, error);
+    },
+  } satisfies FastifyTRPCPluginOptions<AppRouter>["trpcOptions"],
+});
+
+(async () => {
+  try {
+    await server.listen({ port: 3001 });
+  } catch (err) {
+    server.log.error(err);
+    process.exit(1);
+  }
+})();
